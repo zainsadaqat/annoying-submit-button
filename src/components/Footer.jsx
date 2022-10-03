26bfb187d83093f5df05150ae877a3226c81a903
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-10-04 03:11:25 +0500

    Create Footer and Social Media Links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center' }}>
+      All &copy; Rights Reserved!
+      <p style={{ marginTop: '2px' }}>
+        Follow Zain Sadaqat on{' '}
+        <a href="https://www.linkedin.com/in/zain-sadaqat/">LinkedIn</a> and{' '}
+        <a href="https://www.github.com/zainsadaqat/">GitHub</a> for more.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
